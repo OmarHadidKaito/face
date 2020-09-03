6b736be96583e31cfd826903e0a7228837b65cb5
Fouad Khatib <f.khatib@gingco.net>
2020-09-03 16:05:05 +0200

    fixed items centerin

--- src/Components/FileUpload.js
+++ src/Components/FileUpload.js
@@ -1,27 +1,28 @@
-import React from 'react'
+import React from "react";
 
+export const FileUpload = (props) => {
+  const hiddenFileInput = React.useRef(null);
 
-export const FileUpload = props => {
-    const hiddenFileInput = React.useRef(null);
+  const handleClick = (event) => {
+    hiddenFileInput.current.click();
+  };
 
-    const handleClick = event => {
-        hiddenFileInput.current.click();
-    };
-
-    const handleChange = event => {
-        const files = event.target.files;
-        props.onChange(files)
-
-    };
-    return (
-        <>
-            <button onClick={handleClick} className={props.className}>Upload a image</button>
-            <input
-                type="file"
-                ref={hiddenFileInput}
-                value={props.value}
-                onChange={handleChange}
-                style={{ display: 'none' }} />
-        </>
-    );
-}
+  const handleChange = (event) => {
+    const files = event.target.files;
+    props.onChange(files);
+  };
+  return (
+    <>
+      <button onClick={handleClick} className={props.className}>
+        Upload a image
+      </button>
+      <input
+        type="file"
+        ref={hiddenFileInput}
+        value={props.value}
+        onChange={handleChange}
+        style={{ display: "none" }}
+      />
+    </>
+  );
+};
--- src/Pages/AI/AI.js
+++ src/Pages/AI/AI.js
@@ -1,55 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { FileUpload } from "../../Components/FileUpload";
-import { DropZone } from '../../Components/dropzone';
-import face from './image/face2.png';
-import './css/style.css';
+import { DropZone } from "../../Components/dropzone";
+import face from "./image/face2.png";
+import "./css/style.css";
 export default function AI() {
-    const [value, setValue] = useState(null);
+  const [value, setValue] = useState(null);
 
-    function handleChange(newValue) {
-        setValue(newValue);
-    }
+  function handleChange(newValue) {
+    setValue(newValue);
+  }
 
-    return (
-
-        < div className="container" >
-            <div className="row">
-                <div className="col-lg-6 white-text vertical-center">
-                    <div className="AMborder">
-                        <p className="uplouad-paragraph">
-                            Upload an image
-                        </p>
-                        <FileUpload className="button-chooser" onChange={handleChange}></FileUpload>
-                        <hr
-                            style={{
-                                color: "#DAE1E7",
-                                backgroundColor: "#DAE1E7",
-                                height: "1px",
-                                width: "80%",
-                                marginLeft: "10%",
-                                marginRight: "10%"
-                            }}
-                        />
-                        <DropZone className="drop-zone" onChange={handleChange}></DropZone>
-                        <hr
-                            style={{
-                                color: "#DAE1E7",
-                                backgroundColor: "#DAE1E7",
-                                height: "1px",
-                                width: "80%",
-                                marginLeft: "10%",
-                                marginRight: "10%"
-                            }}
-                        />
-                        {value ? <p className="file-paragraph text-center">{value[0].name}</p> : <p className="file-paragraph text-center"><br /></p>}
-                        <button href="# " className="AIbutton justify-content-center ">Start analyzing</button>
-                    </div>
-
-                </div>
-                <div className="col-lg-6 white-text vertical-center">
-                    <img src={face} className="img-fluid" alt="." />
-                </div>
-            </div>
-        </div >
-    )
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-6 white-text vertical-center">
+          <div className="AMborder">
+            <p className="uplouad-paragraph">Upload an image</p>
+            <FileUpload
+              className="button-chooser"
+              onChange={handleChange}
+            ></FileUpload>
+            <hr
+              style={{
+                color: "#DAE1E7",
+                backgroundColor: "#DAE1E7",
+                height: "1px",
+                width: "80%",
+                marginLeft: "10%",
+                marginRight: "10%",
+              }}
+            />
+            <DropZone className="drop-zone" onChange={handleChange}></DropZone>
+            <hr
+              style={{
+                color: "#DAE1E7",
+                backgroundColor: "#DAE1E7",
+                height: "1px",
+                width: "80%",
+                marginLeft: "10%",
+                marginRight: "10%",
+              }}
+            />
+            {value ? (
+              <p className="file-paragraph text-center">{value[0].name}</p>
+            ) : (
+              <p className="file-paragraph text-center">
+                <br />
+              </p>
+            )}
+            <button href="# " className="AIbutton justify-content-center ">
+              Start analyzing
+            </button>
+          </div>
+        </div>
+        <div className="col-lg-6 white-text vertical-center">
+          <img src={face} className="img-fluid" alt="." />
+        </div>
+      </div>
+    </div>
+  );
 }
